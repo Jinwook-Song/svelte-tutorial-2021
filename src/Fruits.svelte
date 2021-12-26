219dc76b62af968bcac39a73f980f2734992aaef
jinwook-song <wlsdnr129@naver.com>
2021-12-26 16:55:36 +0900

    Components and Props

---
+++ src/Fruits.svelte
@@ -0,0 +1,31 @@
+<script>
+    // Props
+    // 외부와의 통로 개념
+    export let fruits
+    export let reverse
+    export let slice
+
+    let computedFruites = []
+    let name = ''
+
+    if(reverse) {
+        computedFruites = [...fruits].reverse()
+        name = 'reverse'
+    } else if (slice) {
+        computedFruites = [...fruits].slice(...slice.split(','))
+        name = `slice ${slice}`
+
+    } else {
+        computedFruites = [...fruits]
+
+    }
+</script>
+
+<h2>
+	Fruits {name}
+</h2>
+<ul>
+	{#each computedFruites as fruit}
+	<li>{fruit}</li>
+	{/each}
+</ul>
