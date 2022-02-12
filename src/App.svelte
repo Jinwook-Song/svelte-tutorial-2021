7aa7b967a6433aefa509f6216eedecb7c266b818
jinwook song <wlsdnr129@naver.com>
2022-02-12 14:44:20 +0900

    debug

--- src/App.svelte
+++ src/App.svelte
@@ -1,34 +1,13 @@
 <script lang="ts">
-  import type { ITodo } from './types/ITodo';
-  import { writable } from 'svelte/store';
-  import Todo from './components/Todo.svelte';
+  let count = 0;
+  let double = 0;
 
-  let title = '';
-  // todos 는 단순 배열이 아닌 store 객체가 됩니다.
-  let todos = writable<ITodo[]>([]);
-
-  function createTodo() {
-    // input 값이 없는 경우 return
-    if (!title.trim()) {
-      title = '';
-      return;
-    }
-    $todos.push({
-      id: Date.now(),
-      title,
-    });
-    $todos = $todos; // 할당을 통해 반응성을 유도합니다.
-    title = ''; // input 값 flush
+  function assgin() {
+    count++;
   }
 </script>
 
-<input
-  type="text"
-  bind:value={title}
-  on:keydown={(e) => e.key === 'Enter' && createTodo()}
-/>
-<button on:click={createTodo}>Create Todo</button>
-
-{#each $todos as todo}
-  <Todo {todos} {todo} />
-{/each}
+{@debug count}
+<button on:click={assgin}>Assign</button>
+<h2>{count}</h2>
+<h2>{double}</h2>
