8bbf4d08d2f8e1b234c30267e5894ee2e53d11f0
jinwook song <wlsdnr129@naver.com>
2022-02-13 12:56:55 +0900

    반응성 구문, tick

--- src/App.svelte
+++ src/App.svelte
@@ -1,23 +1,29 @@
 <script lang="ts">
-  loop1: for (let i = 1; i < 4; i++) {
-    loop2: for (let j = 1; j < 4; j++) {
-      if (i === 2) {
-        break loop1;
-      }
-      console.log(`${i}-${j}`);
-    }
-    console.log('-------');
-  }
+  import { tick } from 'svelte';
 
-  jw: {
-    console.log('a');
-    break jw;
-    console.log('b');
-  }
-
-  // Svelte 반응성 구문
   let count = 0;
+  let double = 0;
+
+  // Label 구문
+  // 반응성 구문 내에 반응성을 갖는 변수 명시
   $: {
-    console.log(count);
+    double = count * 2;
+    console.log('double!', double);
+  }
+  $: double2 = count * 2;
+
+  /* 'count 값 변경된 후 대기 로직이 처리되고,
+   화면이 갱신되어야 반응성 구문($:)이 실행된다.' */
+  async function assgin() {
+    count++;
+    console.time('tiemr');
+    await tick(); // 반응성을 기다림
+    console.timeEnd('tiemr');
+    console.log(double);
   }
 </script>
+
+<button on:click={assgin}>Assing</button>
+<h2>{count}</h2>
+<h2>{double}</h2>
+<h2>{double2}</h2>
